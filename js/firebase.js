5558cbc61e95df46d1fdaf8ced729558dc501365
Anirban Deb <anirbandeb430@gmail.com>
2022-06-02 22:58:10 +0530

    2.0

--- js/firebase.js
+++ js/firebase.js
@@ -147,6 +147,7 @@ async function daySale(mail) {
   const docSnap2 = await getDoc(indirectref);
   let total = 0;
   if (!docSnap1.exists() && !docSnap2.exists()) {
+    document.getElementById(`showBtn-${mail}`).style.display = "none";
     document.getElementById(`day-sale-${mail}`).style.display = "";
     document.getElementById(`day-sale-${mail}`).innerHTML =
       "No sale on " + date;
